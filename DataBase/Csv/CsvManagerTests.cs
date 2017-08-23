b040d5f0a8a5df5deef0609533798e1d8bf8af57
Arnaud-Bouillod <arnaud.bouillod@gmail.com>
2017-08-23 11:06:05 +0200

    Added unit test for ObjectToWrite DataBase

---
+++ DataBase/Csv/CsvManagerTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataBase.Csv;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.DataBase.Entities;
+
+namespace Tests.DataBase.Csv
+{
+    [TestClass()]
+    public class CsvManagerTests
+    {
+        [TestMethod()]
+        public void WriteToCsvFileTest()
+        {
+            Book book = Book.example_book();
+            CsvManager.WriteToCsvFile<Book>(Book.path(), "test.csv", book);
+            string text = System.IO.File.ReadAllText(Book.path() + "test.csv").Replace("\n", "").Replace(" ", "").Replace("\r", "");
+            Assert.AreEqual("0,Djangotutorial,2017,Python,False", text);
+        }
+
+        [TestMethod()]
+        public void ReadFromCsvFileTest()
+        {
+            Book csv_book = CsvManager.ReadFromCsvFile<Book>(Book.path(), "test.csv");
+            Book book = Book.example_book();
+            Assert.AreEqual(book, csv_book);
+        }
+    }
+}
---
+++ DataBase/Xml/XmlManagerTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DataBase.Xml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.DataBase.Entities;
+
+namespace Tests.DataBase.Xml
+{
+    [TestClass()]
+    public class XmlManagerTests
+    {
+        [TestMethod()]
+        public void WriteToXmlFileTest()
+        {
+            Book book = Book.example_book();
+            XmlManager.WriteToXmlFile<Book>(Book.path(), "test.xml", book);
+            String text = System.IO.File.ReadAllText(Book.path() + "test.xml").Replace("\n", "").Replace("\r", "");
+            Assert.AreEqual("<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+                            "<Book xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">" +
+                            "  <BookId>0</BookId>" +
+                            "  <Title>Django tutorial</Title>" +
+                            "  <Year>2017</Year>" +
+                            "  <Author>Python</Author>" +
+                            "  <Is_active>false</Is_active>" +
+                            "</Book>", text);
+        }
+
+        [TestMethod()]
+        public void ReadFromXmlFileTest()
+        {
+            Book xml_book = XmlManager.ReadFromXmlFile<Book>(Book.path(), "test.xml");
+            Book book = Book.example_book();
+            Assert.AreEqual(book, xml_book);
+        }
+    }
+}
